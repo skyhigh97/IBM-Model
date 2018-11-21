0347150192b2939b77f4af632e172509702c9b8a
Akash Dash <akash.dash.vicky@gmail.com>
2018-11-21 23:33:09 +0530

    added functions

--- IBM1.py
+++ IBM1.py
@@ -7,176 +7,146 @@ import time
 import json
 import numpy as np
 
-#load data from json files
-file1 = json.load(open('data1.json'))
-file2 = json.load(open('data2.json'))
 
-#to store sentences
-fr_sentence = []
-en_sentence = []
-
-#to store all  words
-fr_words = []
-en_words = []
-
-#to store translation probability
-#t = {}
-
-#append each sentence into lists
-for i in range(len(file1)):    
-    fr_sentence.append(file1[i]['fr'])
-    en_sentence.append(file1[i]['en'])
-    
-for i in range(len(file2)):    
-    fr_sentence.append(file2[i]['fr'])
-    en_sentence.append(file2[i]['en'])    
-
-
-n = len(en_sentence)
-  
-#store all words in sets 
-for i in range(n):                 
-    en_word = en_sentence[i].split(' ')
-    
-    for tmp in en_word:
-        if(not(tmp in en_words)):
-            en_words.append(tmp)
-    
-    fr_word = fr_sentence[i].split(' ')
-    
-    for tmp in fr_word:
-        if(not(tmp in fr_words)):
-            fr_words.append(tmp)
-           
+def IBM1model(file,x=1000):
+    #load data from json files
     
 
+    #to store sentences
+    fr_sentence = []
+    en_sentence = []
+
+    #to store all  words
+    fr_words = []
+    en_words = []
+
+    #to store translation probability
+    #t = {}
+
+    #append each sentence into lists
+    for i in range(len(file)):    
+        fr_sentence.append(file[i]['fr'])
+        en_sentence.append(file[i]['en'])
+        
+    
+    n = len(en_sentence)
+    
+    #store all words in sets 
+    for i in range(n):                 
+        en_word = en_sentence[i].split(' ')
+        
+        for tmp in en_word:
+            if(not(tmp in en_words)):
+                en_words.append(tmp)
+        
+        fr_word = fr_sentence[i].split(' ')
+        
+        for tmp in fr_word:
+            if(not(tmp in fr_words)):
+                fr_words.append(tmp)
             
-l1 = len(en_words)
-l2 = len(fr_words)
+        
 
-#print(en_words)
-#print(fr_words)
-    
-count = np.zeros(shape=(l1,l2))    # nxn matrix filled with 0s
-total = np.zeros(l1+l2)    #list of size n filled with 0s
-converged = None
-stotal = np.zeros(l1+l2)
-t = np.zeros(shape=(l1,l2))
-
-# initialize t(e|f) uniformly
-for i in range(l1):
-    for j in range(l2):
-        t[i][j] = np.random.uniform(0,1)
-
-'''
-s='\t'
-for j in range(l2):
-    s = s + fr_words[j] + '\t'
-s = s + '\n'
-    
-for i in range(l1):
-    s = s + en_words[i] + ' :\t '
-    for j in range(l2):
-        s = s + str(round(t[i][j],2)) + '\t'
-    s = s + '\n'
-print(s)'''
-    
-x = 2000    
-        
-while( x ):
-    # initialize
-   
-        
-    converged = True    
-    
-    for i in range(n):
-        #compute normalization
-        e =  en_sentence[i].split(' ')
-        f =  fr_sentence[i].split(' ')
-        
-        for en_word in e :
-            e1 = en_words.index(en_word)
-            stotal[e1] = 0
-            
-            for fr_word in f :
-                f1 = fr_words.index(fr_word)
-                stotal[e1] +=  t[e1][f1]
-        
-        # collect counts
-        for en_word in e :
-            for fr_word in f :
-                e1 = en_words.index(en_word)
-                f1 = fr_words.index(fr_word)
                 
-                count[e1][f1] += t[e1][f1]/stotal[e1]
-                total[f1] += t[e1][f1]/stotal[e1]
-    
-    #estimate probabilities
-    for fr_word in fr_words:
-        for en_word in en_words:
-            e1 = en_words.index(en_word)
-            f1 = fr_words.index(fr_word)
-            t[e1][f1] = count[e1][f1]/total[f1]
-            
-    #check for convergence
-    """
+    l1 = len(en_words)
+    l2 = len(fr_words)
+
+    #print(en_words)
+    #print(fr_words)
+        
+    count = np.zeros(shape=(l1,l2))    # l1xl2 matrix filled with 0s
+    total = np.zeros(l1+l2)    #list of size n filled with 0s
+    #converged = False
+    stotal = np.zeros(l1+l2)
+    t = np.zeros(shape=(l1,l2))
+
+    # initialize t(e|f) uniformly
     for i in range(l1):
         for j in range(l2):
-            if( t[i][j] > 0.05 and  t[i][j] < 0.95):
-                converged = False  """
-                
-    x = x-1
+            t[i][j] = np.random.uniform(0,1)
 
-#print(s1)
+
+   
+    while( x ):
+        # initialize
+    
+        #told = t            
+        #converged = True    
+        
+        for i in range(n):
+            #compute normalization
+            e =  en_sentence[i].split(' ')
+            f =  fr_sentence[i].split(' ')
+            
+            for en_word in e :
+                e1 = en_words.index(en_word)
+                stotal[e1] = 0
                 
-s='\t'
-for j in range(l2):
-    s = s + fr_words[j] + '\t'
-s = s + '\n'
+                for fr_word in f :
+                    f1 = fr_words.index(fr_word)
+                    stotal[e1] +=  t[e1][f1]
+            
+            # collect counts
+            for en_word in e :
+                for fr_word in f :
+                    e1 = en_words.index(en_word)
+                    f1 = fr_words.index(fr_word)
+                    
+                    count[e1][f1] += t[e1][f1]/stotal[e1]
+                    total[f1] += t[e1][f1]/stotal[e1]
+        
+        #estimate probabilities
+        for fr_word in fr_words:
+            for en_word in en_words:
+                e1 = en_words.index(en_word)
+                f1 = fr_words.index(fr_word)
+                t[e1][f1] = count[e1][f1]/total[f1]
+                
+        #check for convergence
+        """
+        for i in range(l1):
+            for j in range(l2):
+                if( 100*abs(t[i][j] -told[i][j])/t[i][j] < 1e-5):
+                    converged = False  """
+        
+                    
+        x = x-1
     
-for i in range(l1):
-    s = s + en_words[i] + ' :\t '
-    for j in range(l2):
-        s = s + str(round(t[i][j],2)) + '\t'
+    printData(en_words,fr_words,t)
+
+    return en_words,fr_words,t
+
+
+def printData(en_words, fr_words, t):
+    #print(s1)
+                
+    s='\t'
+    for j in range(len(fr_words)):
+        s = s + fr_words[j] + '\t'
     s = s + '\n'
-print(s)
-    
+        
+    for i in range(len(en_words)):
+        s = s + en_words[i] + ' :\t '
+        for j in range( len(fr_words)):
+            s = s + str(round(t[i][j],3)) + '\t'
+        s = s + '\n'
+    print(s)
+    return
+
+
+# IMPORT dataset
+file1 = json.load(open('data1.json'))
+file2 = json.load(open('data2.json'))
+file3 = json.load(open('hamara_corpus.json'))
+
+
+#get translation probability
+
+en_word1,fr_word1,t1 = IBM1model(file1,2000)
+en_word2,fr_word2,t2 = IBM1model(file2,1000)
+en_word3,fr_word3,t3 = IBM1model(file3,5000)
+
+#printData(en_word1,fr_word1, t1)  
 
       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+       
