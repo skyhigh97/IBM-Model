a097467ad081a4cd860cae5508123a7b749a2531
shubhampaliwal95 <37957134+shubhampaliwal95@users.noreply.github.com>
2018-11-22 16:49:02 +0530

    Add files via upload

--- q2.py
+++ q2.py
@@ -2,6 +2,7 @@ import time
 import json
 import numpy as np
 from nltk.translate import IBMModel1, AlignedSent
+from nltk.translate import IBMModel2, AlignedSent
 
 
 #load data from json files
@@ -27,6 +28,7 @@ n = len(en_sentence)
 
 bitext = []
 
+
 #store all words in sets 
 
 for i in range(n):                 
@@ -34,8 +36,33 @@ for i in range(n):
     fr_word = fr_sentence[i].split(' ')    
     bitext.append(AlignedSent(fr_word, en_word))
 
+
 ibm1 = IBMModel1(bitext, 2000)
+#ibm2 = IBMModel2(bitext, 2000)
+test_sentence = bitext[2]
+print(test_sentence.words)
+#print('\n')
+print(test_sentence.mots)
+#print('\n')
+print("Alignment according to IBM1 nltk model\n")
+print(test_sentence.alignment)
 
-print(ibm1.translation_table['das']['the'])
-print(ibm1.translation_table['der']['the'])
+ibm2 = IBMModel2(bitext, 2000)
+test_sentence = bitext[2]
+print(test_sentence.words)
+#print('\n')
+print(test_sentence.mots)
+#print('\n')
+print("Alignment according to IBM2 nltk model\n")
+print(test_sentence.alignment)
 
+#print(ibm1.translation_table['der']['the'])
+#print(ibm2.translation_table['das']['the'])
+'''for i in range(n):
+	en_word = en_sentence[i].split(' ')
+	for j in range(n):
+		fr_word = fr_sentence[j].split(' ')
+		print(ibm2.translation_table[fr_word][en_word])
+		print('	')
+	print('\n')
+'''
