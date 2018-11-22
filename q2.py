9a548bec7d2541232d1d19a2c302b5fe6268c659
shubhampaliwal95 <37957134+shubhampaliwal95@users.noreply.github.com>
2018-11-22 20:25:07 +0530

    Add files via upload

--- q2.py
+++ q2.py
@@ -6,55 +6,63 @@ from nltk.translate import IBMModel2, AlignedSent
 
 
 #load data from json files
-file = json.load(open('hamara_corpus.json'))
+def IBM1_IBM2(filename):
+	file = json.load(open(filename))
+    #print("Corpus:"filename)
 
-#to store sentences
-fr_sentence = []
-en_sentence = []
+	#to store sentences
+	fr_sentence = []
+	en_sentence = []
 
-#to store all  words
-fr_words = []
-en_words = []
+	#to store all  words
+	fr_words = []
+	en_words = []
 
-#to store translation probability
+	#to store translation probability
 
-#append each sentence into lists
-for i in range(len(file)):    
-    fr_sentence.append(file[i]['fr'])
-    en_sentence.append(file[i]['en'])
+	#append each sentence into lists
+	for i in range(len(file)):    
+	    fr_sentence.append(file[i]['fr'])
+	    en_sentence.append(file[i]['en'])
+	    
+
+	n = len(en_sentence)
+
+	bitext = []
+
+
+	#store all words in sets 
+
+	for i in range(n):                 
+	    en_word = en_sentence[i].split(' ')    
+	    fr_word = fr_sentence[i].split(' ')    
+	    bitext.append(AlignedSent(fr_word, en_word))
+
+
+	ibm1 = IBMModel1(bitext, 2000)
+	#ibm2 = IBMModel2(bitext, 2000)
+	test_sentence = bitext[2]
+	print(test_sentence.words)
+	#print('\n')
+	print(test_sentence.mots)
+	#print('\n')
+	print("Alignment according to IBM1 nltk model\n")
+	print(test_sentence.alignment)
     
-
-n = len(en_sentence)
-
-bitext = []
-
-
-#store all words in sets 
-
-for i in range(n):                 
-    en_word = en_sentence[i].split(' ')    
-    fr_word = fr_sentence[i].split(' ')    
-    bitext.append(AlignedSent(fr_word, en_word))
-
-
-ibm1 = IBMModel1(bitext, 2000)
-#ibm2 = IBMModel2(bitext, 2000)
-test_sentence = bitext[2]
-print(test_sentence.words)
-#print('\n')
-print(test_sentence.mots)
-#print('\n')
-print("Alignment according to IBM1 nltk model\n")
-print(test_sentence.alignment)
-
-ibm2 = IBMModel2(bitext, 2000)
-test_sentence = bitext[2]
-print(test_sentence.words)
-#print('\n')
-print(test_sentence.mots)
-#print('\n')
-print("Alignment according to IBM2 nltk model\n")
-print(test_sentence.alignment)
+	ibm2 = IBMModel2(bitext, 2000)
+	test_sentence = bitext[2]
+	print(test_sentence.words)
+	#print('\n')
+	print(test_sentence.mots)
+	#print('\n')
+    print("Alignment according to IBM2 nltk model\n")
+    print(test_sentence.alignment)
+print("Corpus:hamara_corpus" )
+#IBM1_IBM2('hamara_corpus.json')
+print("Corpus:data1")
+#IBM1_IBM2('data1.json')
+print("Corpus:data2")
+IBM1_IBM2('data2.json')
 
 #print(ibm1.translation_table['der']['the'])
 #print(ibm2.translation_table['das']['the'])
